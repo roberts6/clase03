a1f20d7a1883927f4103221b78e594f1e79d0f30
roberts6 <roberts_oscar@hotmail.com>
2021-01-05 09:07:06 -0600

    estuve haciendo algunas pruebas, sin modificaciones

--- calculadora.js
+++ calculadora.js
@@ -6,10 +6,10 @@ const dividir = require ("./dividir");
 
 const resta = require ("./resta");
 
-console.log (multiplicar(8 , 5));
+console.log (multiplicar(2 , 5));
 
-console.log (resta(8 , 5));
+console.log (resta(8 , 7));
 
-console.log (suma(8 , 5));
+console.log (suma(8 , 8));
 
-console.log (dividir(8 , 5));
+console.log (dividir(8 , 8));
